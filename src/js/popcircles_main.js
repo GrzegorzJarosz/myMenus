89dd6533d18ba64a39956480f0d1bec9ad06c9f3
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2018-05-25 22:27:47 +0200

    menu pop-circles added

--- src/js/popcircles_main.js
+++ src/js/popcircles_main.js
@@ -1,6 +1,24 @@
 
 (function(){
+	const menu = document.querySelector('.pop-c-nav-cont');
+	const particles = document.querySelectorAll('.pop-c-nav-lnk');
+	let activator = false;
 
-	console.log('hello menus');
-
+	if(menu === null){return}
+	menu.addEventListener('click', function(){
+	let pL = particles.length;
+		if(!activator){
+			for(let i = 0 ; i < pL ; i++){
+				const PI = Math.PI
+				let newX = Math.cos(i * ((PI / 2) / 2.8)) * 111;
+				let newY = Math.sin(i * ((PI / 2) / 2.8)) * 111;
+				particles[i].style.transform = "translate("+newX+"px,"+newY+"px)";
+			}
+		}else{
+			for(let i = 0 ; i < pL ; i++){
+				particles[i].style.transform = "none";
+			}
+		}
+		activator = !activator;
+	});
 })();
